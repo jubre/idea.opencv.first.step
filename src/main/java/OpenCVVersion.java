645709b5bcd138ebded394115f6201faf073a7f4
Junior Corman <junior.corman@gmail.com>
2016-12-14 22:00:29 -0500

    Creacion class OpenCVVersion

---
+++ src/main/java/OpenCVVersion.java
@@ -0,0 +1,11 @@
+package main.java;
+
+/**
+ * Created by junior on 14/12/16.
+ */
+public class OpenCVVersion {
+
+    public static void main(String[] args){
+        System.out.println("hello World!");
+    }
+}
