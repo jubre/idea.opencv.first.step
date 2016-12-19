96e2aa67f4f76223005f11d302afe5586ce683e7
Junior Corman <junior.corman@gmail.com>
2016-12-19 15:54:08 -0500

    Update main Class

--- src/main/java/hello/OpenCVMain.java
+++ src/main/java/hello/OpenCVMain.java
@@ -1,13 +1,11 @@
 package hello;
 
-//import org.opencv.core.Core;
-
 /**
  * Created by junior on 15/12/16.
  */
 public class OpenCVMain {
 
     public static void main(String[] args) {
-        System.out.println("hello World! + OpenCV! version: ");
+        System.out.println("hello World! + OpenCV! version: " + new OpenCVVersion().getOpenCVVersion());
     }
 }
